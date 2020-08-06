802702f18ec4fa9c58fcfdbb813997793c1a37c6
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-06 11:23:53 +0300

    Первый урок

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-let money = 10, 
-income = 100, 
-addExpenses = -100, 
-deposit = 1.5, 
-mission = {
-    name: 'Урок 1',
-    target: 'Введение в язык, подключение javascript. Основы работы с Git',
-    term: 1 + ' час' 
-}, 
-period = 1 / 5;
+let money, 
+income, 
+addExpenses, 
+deposit, 
+mission, 
+period;
 
 alert('Hello');
 console.log('Hello');
-
-console.log(money);
-console.log(income);
-console.log(addExpenses);
-console.log(deposit);
-console.log(mission);
-console.log(period);
