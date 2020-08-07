ad368dbb8730bfc6ca4fa919f17f2446af227469
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-07 09:18:18 +0300

    ДЗ по второму уроку

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-// let money, 
-// income, 
-// addExpenses, 
-// deposit, 
-// mission, 
-// period;
+let money, 
+income, 
+addExpenses, 
+deposit, 
+mission, 
+period;
 
-// alert('Hello');
-// console.log('Hello');
+money = 40000;
+income = 'Фриланс';
+addExpenses = 'Кафе, метро, интернет, сотовая связь, коммуналка';
+deposit = true;
+mission = 200000;
+period = 10;
+
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log('Длинна строки addExpenses: ', addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
+
