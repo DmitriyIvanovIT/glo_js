d55b41095347c0a742ce6649715023d1eb9b1962
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-10 09:31:32 +0300

    feat: Creating a calculator | /script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+'use strict';
+
 let money = 40000,
 income = 'Фриланс',
 addExpenses = 'Кафе, метро, интернет, сотовая связь, коммуналка',
 deposit = true,
 mission = 200000,
 period = 10,
-budgetDay = money / 30;
+budgetDay = money / 30,
+budgetMonth;
 
 console.log('money: ', typeof money);
 console.log('income: ', typeof income);
@@ -15,3 +18,22 @@ console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
 console.log('budgetDay: ', budgetDay);
 
+money = +prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
+
+budgetMonth = money - (amount1 + amount2);
+
+console.log(Math.ceil(mission / budgetMonth));
+
+budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+if (budgetDay >= 1200) console.log('У вас высокий уровень дохода') 
+else if (budgetDay >= 600 && budgetDay < 1200) console.log('У вас средний уровень дохода') 
+else if (budgetDay < 600 || budgetDay === 0) console.log('У вас средний уровень дохода')
+else if (budgetDay < 0) console.log('Что то пошло не так'); 
