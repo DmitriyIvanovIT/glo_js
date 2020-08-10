3eb365de0ad41d8db86f8a12c47e29d8d8c57893
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-10 09:41:04 +0300

    feat: Creating a calculator | /script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 'use strict';
 
-let money = 40000,
+let money = +prompt('Ваш месячный доход?'),
 income = 'Фриланс',
-addExpenses = 'Кафе, метро, интернет, сотовая связь, коммуналка',
-deposit = true,
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
 mission = 200000,
 period = 10,
-budgetDay = money / 30,
-budgetMonth;
+expenses1 = prompt('Введите обязательную статью расходов?'),
+amount1 = +prompt('Во сколько это обойдется?'),
+expenses2 = prompt('Введите обязательную статью расходов?'),
+amount2 = +prompt('Во сколько это обойдется?'),
+budgetMonth = money - (amount1 + amount2),
+budgetDay = budgetMonth / 30;
 
 console.log('money: ', typeof money);
 console.log('income: ', typeof income);
@@ -16,22 +20,9 @@ console.log('Длинна строки addExpenses: ', addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
-console.log('budgetDay: ', budgetDay);
-
-money = +prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount1 = +prompt('Во сколько это обойдется?');
-let amount2 = +prompt('Во сколько это обойдется?');
-
-budgetMonth = money - (amount1 + amount2);
-
-console.log(Math.ceil(mission / budgetMonth));
-
-budgetDay = budgetMonth / 30;
-console.log(Math.floor(budgetDay));
+console.log('Бюджет на месяц: ', budgetMonth);
+console.log('Цель будет достигнута за: ', Math.ceil(mission / budgetMonth));
+console.log('Бюджет на день: ', Math.floor(budgetDay));
 
 if (budgetDay >= 1200) console.log('У вас высокий уровень дохода') 
 else if (budgetDay >= 600 && budgetDay < 1200) console.log('У вас средний уровень дохода') 
