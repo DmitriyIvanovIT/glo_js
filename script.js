a9044eb68117bbb27221aa48041aed6058e82772
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-17 15:12:22 +0300

    feat: Project preparation | /script.js

--- script.js
+++ script.js
@@ -14,9 +14,17 @@ let isNumber = n => {
         return money = +money;
     },
     isString = n => {
+        let check = true;
         if (n !== null) {
             if (n.trim().length > 0 && !isNumber(n)) {
-                return true;
+                for (let i = 0; i< n.length; i++) {
+                    if (isNumber(n[i])) {
+                        check = false;
+                    };
+                };
+                if (check) {
+                    return true;
+                } 
             }
         }
 
