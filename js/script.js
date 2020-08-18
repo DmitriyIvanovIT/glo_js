5f0991ba5af3a0d43719bbd6b2e31da3805159d8
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-18 16:08:07 +0300

    feat: Working with DOM elements | /index.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 'use strict';
+const startButton = document.getElementById('start'),
+incomeAdd = document.getElementsByTagName('button')[0],
+expensesAdd = document.getElementsByTagName('button')[1],
+depositCheck = document.querySelector('#deposit-check'),
+additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
+budgetMonthValue = document.getElementsByClassName('result-total')[0],
+budgetDayValue = document.getElementsByClassName('result-total')[1],
+expensesMonthValue = document.getElementsByClassName('result-total')[2],
+additionalIncomeValue = document.getElementsByClassName('result-total')[3],
+additionalExpensesValue = document.getElementsByClassName('result-total')[4],
+incomePeriodValue = document.getElementsByClassName('result-total')[5],
+targetMonthValue = document.getElementsByClassName('result-total')[6],
+salaryAmount = document.querySelector('.salary-amount'),
+incomeTitle = document.querySelector('input.income-title'),
+incomeAmount = document.querySelector('.income-amount'),
+expensesTitle = document.querySelector('input.expenses-title'),
+expensesAmount = document.querySelector('.expenses-amount'),
+additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+range = document.querySelector('[type="range"]');
+
 let money;
 
 let isNumber = n => {
@@ -157,3 +177,23 @@ for (let key in appData) {
 };
 
 convertString(appData.addExpenses);
+
+console.log(startButton);
+console.log(incomeAdd);
+console.log(expensesAdd);
+console.log(depositCheck);
+console.log(additionalIncomeItem);
+console.log(budgetMonthValue);
+console.log(budgetDayValue);
+console.log(expensesMonthValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(range);
